4f22708659e82ef15245793bf9bd5f7097331915
M-Abbas2208b <abbas2208b@aptechsite.net>
2025-01-03 22:51:40 +0500

    add class 18

---
+++ Classes/class18/src/app/page.tsx
@@ -0,0 +1,16 @@
+// hooks events
+// Building forms with React Hook Form and Zod.
+
+
+import ContactForm from '@/components/ui/contact-form'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <ContactForm />
+    </div>
+  )
+}
+
+export default Home
---
+++ Classes/class18/src/sanity/schemaTypes/index.ts
@@ -0,0 +1,6 @@
+import { type SchemaTypeDefinition } from 'sanity'
+import contactform from './contactform'
+
+export const schema: { types: SchemaTypeDefinition[] } = {
+  types: [contactform],
+}
